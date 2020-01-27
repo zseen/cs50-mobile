cb6f742d4f60781a04046904eeb7fa2020cef89b
zseen <43151120+zseen@users.noreply.github.com>
2020-01-27 12:50:08 +0000

    Display todo text and enable checking the checkbox

--- Project0/project0/project0/script.js
+++ Project0/project0/project0/script.js
@@ -9,19 +9,52 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let itemCount = 0;
+let uncheckedCount = 0;
+
+function renderCounters() {
+  uncheckedCountSpan.innerHTML = uncheckedCount;
+  itemCountSpan.innerHTML = itemCount;
+}
+
 function newTodo() {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
   const todoText = prompt("Enter TODO");
+  
+  itemCount += 1;
+  uncheckedCount += 1;
+
+  
   var li = document.createElement("li");
-  let span = document.createElement('span');
- 
- 
-  li.appendChild(document.createTextNode(' ' + todoText));
+  li.appendChild(input);
+  
+  const div = document.createElement('div');
+  div.textContent = todoText;
+  li.appendChild(div);
+  
+  const button = document.createElement('button');
+  button.setAttribute('title', 'delete');
+  button.textContent = 'delete';
+  
+  button.addEventListener('click', () => {
+    const confirmation = confirm('Are you sure you want to delete this Todo?')
+    if (confirmation) {
+      if (!li.childNodes[0].checked) {
+        uncheckedCount -= 1;
+      }
+      li.parentNode.removeChild(li);
+      itemCount -= 1;
+      renderCounters();
+			}
+		}
+	)
+  
+  li.appendChild(button);
   
   list.appendChild(li);
- 
-  
- 
-  
-  
-  
+  renderCounters();
+
 }
+  
+

959dc5db38406fd4c290c0f6f5a5bfabc430db63
zseen <43151120+zseen@users.noreply.github.com>
2020-03-15 17:54:01 +0000

    Start working on the app.

--- project2/Movies.js
+++ project2/Movies.js
@@ -6,17 +6,12 @@ export default class App extends React.Component {
       moviesList: [],
     };
   
-    updateSearch = search => {
-      this.setState({ search });
-    };
   
     showMoviesList = () => {
       return (
-        <SearchBar
-          placeholder="Type Here..."
-          onChangeText={this.updateSearch}
-          value={search}
-        />
+        <View>
+          //moviesList
+        </View>
       );
     }
   
